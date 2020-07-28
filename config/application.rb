d40657fec12b0255556b86c832ac43ab44fc5428
mohuneko <b0wdmohu@gmail.com>
2020-07-29 02:20:13 +0900

    [finish]test application

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module RspecTest
   config.time_zone = 'Tokyo'
   config.active_record.default_timezone = :local
 
-  config.i18n.default_locale = :js
+  config.i18n.default_locale = :ja
   end
 end
